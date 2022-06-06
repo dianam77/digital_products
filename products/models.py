42e668fee2d33c0f1d48f43e7e13880e44c540b6
unknown <diana.mirrezaee@gmail.com>
2022-06-06 13:40:41 +0430

    Products models added .

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import ungettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, ungettext_lazy
 
 
 class Category(models.Model):
