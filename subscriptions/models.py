918c94ebb50957f1a7f2811d48183d0e452c764e
unknown <diana.mirrezaee@gmail.com>
2022-06-08 14:23:23 +0430

    modified  models added .

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,38 +1,59 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from users.models import User
-from utils.validators import validate_sku
+from utils.validators import validate_phone_number
 
 
-class Package(models.Model):
+class Gateway(models.Model):
     title = models.CharField(_('title'), max_length=50)
-    sku = models.CharField(_('stock keeping unit'), max_length=20, validators=[validate_sku], db_index=True)
     description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='packages/')
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='gateways/')
     is_enable = models.BooleanField(_('is enable'), default=True)
-    price = models.PositiveIntegerField(_('price'))
-    duration = models.DurationField(_('duration'), blank=True, null=True)
-    # gateways = models.ManyToManyField('payments.Gateway')
+    # credentials = models.TextField(_('credentials'), blank=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
 
     class Meta:
-        db_table = 'packages'
-        verbose_name = _('Package')
-        verbose_name_plural = _('Packages')
-
-    def __str__(self):
-        return self.title
+        db_table = 'gateways'
+        verbose_name = _('Gateway')
+        verbose_name_plural = _('Gateways')
 
 
-class Subscription(models.Model):
-    user = models.ForeignKey('users.User', related_name='%(class)s', on_delete=models.CASCADE)
-    package = models.ForeignKey(Package, related_name='%(class)s', on_delete=models.CASCADE)
-    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
-    expire_time = models.DateTimeField(_('expire time'), blank=True, null=True)
+class Payment(models.Model):
+    STATUS_VOID = 0
+    STATUS_PAID = 10
+    STATUS_ERROR = 20
+    STATUS_CANCELED = 30
+    STATUS_REFUNDED = 31
+    STATUS_CHOICES = (
+        (STATUS_VOID, _('Void')),
+        (STATUS_PAID, _('Paid')),
+        (STATUS_ERROR, _('Error')),
+        (STATUS_CANCELED, _('User Canceled')),
+        (STATUS_REFUNDED, _('Refunded')),
+    )
+
+    STATUS_TRANSLATIONS = {
+        STATUS_VOID: _('Payment could not be processed'),
+        STATUS_PAID: _('Payment successful'),
+        STATUS_ERROR: _('Payment has encountered an error. Our technical team will check the problem shortly'),
+        STATUS_CANCELED: _('Payment canceled by user.'),
+        STATUS_REFUNDED: _('This payment has been refunded'),
+    }
+
+    user = models.ForeignKey('users.User', verbose_name=_('user'), related_name='%(class)s', on_delete=models.CASCADE)
+    package = models.ForeignKey('subscriptions.Package', verbose_name=_('package'), related_name='%(class)s', on_delete=models.CASCADE)
+    gateway = models.ForeignKey(Gateway, verbose_name=_('gateway'), related_name='%(class)s', on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(_('price'), default=0)
+    status = models.PositiveSmallIntegerField(_('status'), choices=STATUS_CHOICES, default=STATUS_VOID, db_index=True)
+    device_uuid = models.CharField(_('device uuid'), max_length=40, blank=True)
+    token = models.CharField()
+    phone_number = models.BigIntegerField(_('phone number'), validators=[validate_phone_number], db_index=True)
+    consumed_code = models.PositiveIntegerField(_('consumed reference code'), null=True, db_index=True)
+    created_time = models.DateTimeField(_('creation time'), auto_now_add=True, db_index=True)
+    updated_time = models.DateTimeField(_('modification time'), auto_now=True)
 
     class Meta:
-        db_table = 'subscriptions'
-        verbose_name = _('Subscription')
-        verbose_name_plural = _('Subscriptions')
+        db_table = 'payments'
+        verbose_name = _('Payment')
+        verbose_name_plural = _('Payments')
